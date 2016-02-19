24119492b05f0e2488e92c4502b079707d496fb3
Narendra-mindstix <dilip.kumar@mindstix.com>
2016-02-19 19:44:02 +0530

    commited on 18-feb

---
+++ controller/app.js
@@ -0,0 +1,34 @@
+var app=angular.module("myApp",['ngRoute']);
+
+/*Get all data from the json file */
+app.controller('myCtrl1',function($scope,$http){
+    $http.get('jsonFiles/angular.json').success(function(response){
+        $scope.topics=response;
+    });
+});
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/Angular Intro', {
+		   templateUrl: 'tutorials/angularjs/Angular Intro.html'
+		}).
+		when('/Angular Expression', {
+		   templateUrl: 'tutorials/angularjs/Angular Expression.html'
+		}).
+		when('/Angular Intro', {
+		   templateUrl: 'tutorials/angularjs/angular introduction.html'
+		}).
+		when('/Angular Directives', {
+		   templateUrl: 'tutorials/angularjs/Angular Directives.html'
+		}).
+		when('/Angular Intro', {
+		   templateUrl: 'tutorials/angularjs/angular introduction.html'
+		}).
+		when('/Angular Intro', {
+		   templateUrl: 'tutorials/angularjs/angular introduction.html'
+		}).
+		when('/Angular Filters', {
+		   templateUrl: 'tutorials/angularjs/Angular Filters.html'
+		});
+
+ }]);
+ 
